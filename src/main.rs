4e35f6afd049dc5d8826ef7d601607f927d68bed
ChildishSky2 <106922566+ChildishSky2@users.noreply.github.com>
2024-10-04 15:21:59 +0100

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+mod rubix_cube;
+//use rubix_cube::Iddfs;
+use rubix_cube::iddfs;
+fn main() {
+    let mut rubix = rubix_cube::RubixCube::create_solved_rubix();
+
+    rubix.make_random_moves(9);
+    //rubix.search_astar();
+    rubix.thread_search_iddfs(10);
+    //rubix.print_cube();
+
+}
+    
